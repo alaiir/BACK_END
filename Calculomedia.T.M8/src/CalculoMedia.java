0094e6194db15189db1f4b0fca1341f047d1433b
alaiir <99047113+alaiir@users.noreply.github.com>
2024-02-20 20:33:07 -0300

    Add files via upload
    tarefa calculadora media

---
+++ Calculomedia.T.M8/src/CalculoMedia.java
@@ -0,0 +1,24 @@
+/**
+ * @author alair
+ */
+
+public class CalculoMedia {
+
+    public static void main(String args[]) {
+        notasProvas();
+    }
+ public static void notasProvas() {
+
+
+
+     int prova = 9;
+     int execicio = 7;
+     int tarefa = 5;
+     double redacao = 6.5;
+
+     double total = (9 + 7 + 5 + 6.5)/4;
+     System.out.println(total);
+
+ }
+
+}
